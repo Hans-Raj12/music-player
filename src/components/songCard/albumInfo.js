eaf33ca688ee79ca1e94e9a71cbe2fe949a7e48e
Kashish Kirplani <kashish.bscsf19@iba-suk.edu.pk>
2022-08-08 12:38:00 +0500

    added the css on albumImage component card and also added css on the albumInfo component card inside Player component

--- src/components/songCard/albumInfo.js
+++ src/components/songCard/albumInfo.js
@@ -1,5 +1,23 @@
 import React from "react";
+import './albumInfo.css'
+export default function AlbumInfo({album}){
+    console.log(album)
+    const artists = []
+    album?.artists?.forEach((artist)=>{
+        artists.push(artist.name)
+    })
+    return (
+        <div className="albumInfo-card">
+            <div className="albumName-container">
+                <p>{album?.name+"-"+artists?.join(", ")}</p>
+            </div>
+            <div className="album-info">
+                <p>{`${album?.name} is an ${album?.album_type} by ${artists?.join(", ")} with ${album?.total_tracks } Track(s)`}</p>
+            </div>
+            <div className="album-release">
+                <p>Release Date: {album?.release_date} </p>
+            </div>
+        </div>
+    )
 
-export default function AlbumInfo(){
-    return <div>AlbumInfo</div>
 }
--- src/components/songCard/index.js
+++ src/components/songCard/index.js
@@ -3,7 +3,7 @@ import AlbumImage from "./albumImage.js";
 import AlbumInfo from "./albumInfo.js";
 import './songCard.css'
 export default function SongCard({album}){
-    return <div className="songCard-body">
+    return <div className="songCard-body flex">
         <AlbumImage url={album?.images[0]?.url}/>
         <AlbumInfo album={album}/>
     </div>
